92efb28b83de63cf844ad79592b3663e5dc0c18b
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2018-04-08 08:31:47 +0800

    Menggunakan Method Set

--- src/latihan03/AirMineralAksi.java
+++ src/latihan03/AirMineralAksi.java
@@ -2,10 +2,11 @@ package latihan03;
     public class AirMineralAksi {
         public static void main(String[] args) {
              AirMineral am = new AirMineral();
-             am.merk="Vit";
-             am.jenis="Gelas";
-             am.berat=240;
-             am.harga=18000;
+             
+             am.setMerk("Vit");
+             am.setJenis("Gelas");
+             am.setBerat(240);
+             am.setHarga(18000);
              
              am.cetakinfo();
     }
