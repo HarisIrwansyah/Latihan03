9849e3bd237240ef70c1a3f7ed42f8eaa725c3a4
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2018-04-08 09:06:29 +0800

    Attribute dan Method

--- src/latihan03/AirMinumKemasan.java
+++ src/latihan03/AirMinumKemasan.java
@@ -1,5 +1,9 @@
 package latihan03;
 
 public class AirMinumKemasan extends AirMineral{
+    private String jenisAirMinum;
     
+    void throttle(){
+      System.out.println("Dari Mata Air Pilihan, Gunung Batuah");
+    }
 }
